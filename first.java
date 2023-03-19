6eb9136399c5e9e059f14fac5a62dc30b117d59b
Oarabile Ntlhe <oarabile.brett@gmail.com>
2023-03-19 14:08:36 +0200

    added a for loop

--- first.java
+++ first.java
@@ -5,5 +5,10 @@ public class first
         System.out.println("Hello world");
 
         System.out.println("From another branch");
+
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i + "\n");
+        }
     }
 }
